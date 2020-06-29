7ed745ec12bf53d158e1a0782ae6e2af442a09e8
Andrew <58367347+AndrewStru@users.noreply.github.com>
2020-06-29 17:01:07 +0300

    first commit

---
+++ app/src/components/editor/editor.js
@@ -0,0 +1,109 @@
+import "../../helpers/IframeLoader.js";
+import axios from 'axios';
+import React, {Component} from 'react';
+
+export default class Editor extends Component {
+	constructor() {
+		super();
+		this.currentPage = "index.html";
+		this.state = {
+			pageList: [],
+			newPageName: ""
+		}
+		this.createNewPage = this.createNewPage.bind(this);
+	}
+
+	componentDidMount() {
+		this.init(this.currentPage);
+	}
+
+	init(page) {
+		this.iframe = document.querySelector('iframe');
+		this.open(page);
+		this.loadPageList();
+	}
+
+	open(page) {
+		this.currentPage = `../${page}?rnd=${Math.random()}`;
+
+		axios
+			.get(`../${page}`)
+			.then(res => console.log(this.parseStrToDOM(res.data)))
+			
+		// this.iframe.load(this.currentPage, () => {
+		// 	const body = this.iframe.contentDocument.body;
+		// 	let textNodes = [];
+
+		// 	function recursy(element) {
+		// 		element.childNodes.forEach(node => {
+					
+		// 			if(node.nodeName === "#text" && node.nodeValue.replace(/\s+/g, "").length > 0) {
+		// 				textNodes.push(node);
+		// 			} else {
+		// 				recursy(node);
+		// 			}
+		// 		})
+		// 	};
+
+		// 	recursy(body);
+
+		// 	textNodes.forEach(node => {
+		// 		const wrapper = this.iframe.contentDocument.createElement('text-editor');
+		// 		node.parentNode.replaceChild(wrapper, node);
+		// 		wrapper.appendChild(node);
+		// 		wrapper.contentEditable = "true";
+		// 	});
+
+			
+		// });
+		
+		
+	}
+
+	parseStrToDOM(str) {
+		const parser = new DOMParser();
+		return parser.parseFromString(str, "text/html");
+	}
+
+	loadPageList() {
+		axios
+			.get("./api")
+			.then(res => this.setState({pageList: res.data}))
+	}
+
+	createNewPage() {
+		axios
+			.post("./api/createNewPage.php", {"name": this.state.newPageName})
+			.then(this.loadPageList())
+			.catch(() => alert("Страница уже существует!"));
+	}
+
+	deletePage(page) {
+		axios
+			.post("./api/deletePage.php", {"name": page})
+			.then(this.loadPageList())
+			.catch(() => alert("Страницы уже нет на сервере!"));
+	}
+
+	render() {
+		// const {pageList} = this.state;
+		// const pages = pageList.map((page, i) => {
+		// 	return (
+		// 	<h1 key={i}>{page}
+		// 		<a href="#" onClick={() => this.deletePage(page)}>(x)</a>
+		// 	</h1>
+		// 	)
+		// })
+
+		return (
+			<iframe src={this.currentPage} frameBorder="0"></iframe>
+			// <>
+			// 	<input onChange={(e) => {this.setState({newPageName: e.target.value})}} type="text"/>
+
+			// 	< button onClick={this.createNewPage}>Создать страницу</button>
+
+			// 	{pages}
+			// </>
+		)
+	}
+}
